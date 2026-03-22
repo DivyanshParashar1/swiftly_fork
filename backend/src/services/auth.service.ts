e51c12d3d75680184b8f1ec3efe090d350cc7708
nakshjoshi <nakshjoshi2004@gmail.com>
2026-03-22 14:11:57 +0530

    googleAuth backend

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { log } from "node:console";
 import prisma from "../config/prisma";
 import type { createUserInput } from "../types/auth.types";
 import { ApiError } from "../utils/apiError.utils";
@@ -26,72 +27,183 @@ export class AuthService{
     public async createUser(data:createUserInput){
 
 
-        if(data.hashedPassword){
+
+        if(data.provider=="credentials"){            
+
+            if(data.hashedPassword){
                 let newHashedPassword = await hashPassword(data.hashedPassword)
                 data.hashedPassword = newHashedPassword
             }
 
-        return await prisma.$transaction(async(tx)=>{
-
+            return await prisma.$transaction(async(tx)=>{
 
             const existingUser = await tx.user.findUnique({
                 where:{email:data.email}
             })
 
+            
+
             if(existingUser){
-                throw new ApiError(409, "User already exists")
+                
+                const authAccountCrendential = await tx.authAccount.findUnique({
+                    where:{
+                        userId_provider:{
+                            userId: existingUser.id,
+                            provider: "credentials"
+                        }
+                    }
+                })
+
+                if(authAccountCrendential){
+                    throw new ApiError(409, "User already exists with same email id")
+                }
+                
+                const authAccountGoogle = await tx.authAccount.findUnique({
+                    where:{
+                        userId_provider:{
+                            userId: existingUser.id,
+                            provider: "google"
+                        }
+                    }
+                })
+
+                if(authAccountGoogle){
+                    await tx.authAccount.create({
+                        data:{
+                            userId: existingUser.id,
+                            provider:"credentials",
+                            passwordHash: data.hashedPassword
+                        }
+                    })
+
+                    return existingUser
+                }
+
+                throw new ApiError(409, "User already exists with same email id")
             }
 
-
-
-
-
+            
             //create user
 
-            const user = await tx.user.create({
+            const Newuser = await tx.user.create({
                 data:{
                     email: data.email,
                     fullName: data.fullName,
-                    phone:data.phone
+                    phone:data.phone,
+                    avatarUrl:"https://static.vecteezy.com/system/resources/previews/021/548/095/non_2x/default-profile-picture-avatar-user-avatar-icon-person-icon-head-icon-profile-picture-icons-default-anonymous-user-male-and-female-businessman-photo-placeholder-social-network-avatar-portrait-free-vector.jpg"
 
                 }
             })
 
 
-            // create auth
-
-
-            
-
-
-            if(data.provider=="credentials"){
-
-                await tx.authAccount.create({
+            // create auth account with credentials
+            await tx.authAccount.create({
                     data:{
-                        userId: user.id,
+                        userId: Newuser.id,
                         provider:"credentials",
                         passwordHash: data.hashedPassword
                     }
                 })
 
-            } else{
-                await tx.authAccount.create({
-                    data:{
-                        userId: user.id,
-                        provider: data.provider,
-                        providerId:data.providerId
-                    }
-                })
-            }
-
-
-
-
-            return user
+            return Newuser
 
         })
 
+        }
 
+        if(data.provider=="google"){
+
+            
+
+
+            return await prisma.$transaction(async(tx)=>{
+
+
+                const existingUser = await tx.user.findUnique({
+                where:{email:data.email}
+                })
+
+            if(existingUser){                
+
+
+                //check if credentials auth exists for existing user
+                const authAccountCrendential = await tx.authAccount.findUnique({
+                    where:{
+                        userId_provider:{
+                            userId: existingUser.id,
+                            provider: "credentials"
+                        }
+                    }
+                })
+
+                //check if google auth exists for existing user
+                const authAccountGoogle = await tx.authAccount.findUnique({
+                    where:{
+                        userId_provider:{
+                            userId: existingUser.id,
+                            provider: "google"
+                        }
+                    }
+                })
+
+                if(authAccountCrendential && !authAccountGoogle){
+
+                    //add a row with google auth for existing user
+                    const newGoogleAuthForExistingUser = await tx.authAccount.create({
+                        data:{
+                            userId: existingUser.id,
+                            provider:"google",
+                            providerId: data.providerId
+                        }
+                    })
+
+
+                    //update avatarurl with google avatar
+                    const updateAvatar = await tx.user.update({
+                        where:{id:existingUser.id},
+                        data:{avatarUrl:data.avatar}
+                    })
+
+                    return existingUser
+                }
+                
+                
+
+                //if google auth already exists, return existing user
+                if(authAccountGoogle){
+                    return existingUser
+                }
+
+                throw new ApiError(409, "User already exists with same email id")
+            }
+
+            //create user
+            
+            const Newuser = await tx.user.create({
+                data:{
+                    email: data.email,
+                    fullName: data.fullName,
+                    avatarUrl:data.avatar,
+
+                }
+            })
+
+
+            // create auth account with google
+            await tx.authAccount.create({
+                    data:{
+                        userId: Newuser.id,
+                        provider:"google",
+                        providerId: data.providerId
+                    }
+                })
+
+            return Newuser
+
+        })
+            
+            
+        }
     }
 
 
