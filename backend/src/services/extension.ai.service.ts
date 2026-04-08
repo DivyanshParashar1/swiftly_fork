bf7b0ae9a668c98b1a061d9f1d420e0f91e718f6
nakshjoshi <nakshjoshi2004@gmail.com>
2026-04-09 01:02:28 +0530

    gemini model change with higher RPD

--- backend/src/services/extension.ai.service.ts
+++ backend/src/services/extension.ai.service.ts
@@ -134,7 +134,7 @@ Return mapping for ALL keys. If unsure, return null.
       const prompt:string = this.promptBuilder(resumeData, htmlObjectData)
         
         const response = await gemini.models.generateContent({
-            model:'gemini-2.5-flash-lite',
+            model:'gemini-3.1-flash-lite',
             contents: prompt,
             config:{
                 responseMimeType: "application/json",
